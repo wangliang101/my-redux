3126acd37852715e7432a97a64f202cf410a56b7
wliang <wliang@infervision.com>
2021-06-05 22:34:09 +0800

    [add] 全局state读写

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-https://docs.qq.com/doc/DVVFBUnFibnJCRUZS?disableReturnList=1
 import React, { useState, useContext } from 'react';
 
 const appContext = React.createContext(null);
@@ -10,8 +9,39 @@ export function App() {
 
   const contextValue = {appState, setAppState}
   return (
-    <appContext.Provider value={appState}>
-      111
+    <appContext.Provider value={contextValue}>
+      <CompA />
+      <CompB />
+      <CompC />
     </appContext.Provider>
+  )
+}
+
+const CompA = () => <section>子组件A<User/></section>
+const CompB = () => <section>子组件B<UserModifier /></section>
+const CompC = () => <section>子组件C</section>
+
+const User = () => {
+  const contextValue = useContext(appContext)
+  return (
+    <div>
+      User: {contextValue.appState.user.name}
+    </div>
+  )
+}
+
+const UserModifier = () => {
+  const {appState, setAppState} = useContext(appContext);
+  const onChange = (e) => {
+    appState.user.name = e.target.value;
+    setAppState({ ...appState})
+  }
+  return (
+    <div>
+      <input 
+        value={appState.user.name}
+        onChange={onChange}
+      />
+    </div>
   )
 }
